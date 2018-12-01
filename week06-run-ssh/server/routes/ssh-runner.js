e3c8e140f93e54f711c94245d7b6226de290a008
Your Name <you@example.com>
2018-11-30 22:51:01 -0800

    no longer coding IP and private key in week06-sshrun, midterm, and week09-systemrefactordetails

--- week06-run-ssh/server/routes/ssh-runner.js
+++ week06-run-ssh/server/routes/ssh-runner.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const Client = require('ssh2').Client;
 
+const elfUtils = require('elven-code').elfUtils;
 const hostAddress = '35.175.4.96';
 
 let allData = '';
@@ -33,16 +34,56 @@ const runCpuInfo = (hostAddress, response) => {
                 });
         });
     }).connect({
-        ost: hostAddress,        port: 22,
+        host: hostAddress,
+        port: 22,
         username: 'ubuntu',
         privateKey: require('fs').readFileSync(
-            process.env.HOME + '/.ssh/isit320-ec2-rackley'
+            process.env.HOME + '/.ssh/isit320-rackley.pem'
         )
     });
 };
 
+const getSshIp = () => {
+    return new Promise(function (resolve, reject) {
+        elfUtils.readFile(process.env.HOME + '/.ssh/config')
+            .then((content) => {
+                //var pattern = new RegExp('Host ec2-bc[\\s\\S]\\s*(.*)[\\s\\S]\\s*(.*)[\\s\\S]\\s*(.*)[\\s\\S]\\s*(.*)');
+                var pattern = new RegExp('Host ec2-bc\n\t(.*)\n\t(.*)\n\t(.*)\n\t(.*)');
+                const result = {};
+                const match = content.result.match(pattern);
+                for (let i = 1; i < 5; i++) {
+                    if (match[i].startsWith('HostName')) {
+                        var hostPattern = new RegExp('HostName\\s(.*)');
+                        result.hostName = match[i].match(hostPattern)[1];
+                    }
+                    if (match[i].startsWith('IdentityFile')) {
+                        const idPattern = new RegExp('IdentityFile\\s(.*)');
+                        const path = match[i].match(idPattern)[1];
+                        result.identityFile = path.substring(path.lastIndexOf('/') + 1, path.length)
+                    }
+                }
+                resolve(result);
+            })
+            .catch(reject);
+    });
+};
+
+router.get('/uptime', function(request, response) {
+    console.log('run-get-started called in ssh-runner', hostAddress);
+    getSshIp()
+        .then((result) => {
+            runUptime(result.hostName, result.identityFile, response);
+        })
+        .catch((err) => {
+            response.send(err);
+        });
+
+});
+
+
+
 router.get('/call-cpu-info', (request, response) => {
-    response.send({result: 'success'});
+    response.send({ result: 'success' });
 });
 
 runCpuInfo(hostAddress, response);
