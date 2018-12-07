d26940935ded9a6b473fe1ba30fb9d2877986680
Your Name <you@example.com>
2018-12-06 23:44:19 -0800

    working on aws-provision final

--- aws-provision/server/routes/index.js
+++ aws-provision/server/routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const qs = require('query-string');
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -16,17 +19,68 @@ router.get('/foo', function(request, response) {
 
 //createEducate
 router.get('/create-educate', function(request, response) {
-    response.send({ result: 'success' });
-});
+    response.send({
+        result: 'success',
+        route: '/create-educate',
+        instanceData: {
+            InstanceId,
+            KeyName, 
+            Architecture 
+        },
+        allocationIds: {
+            educate: 'educate'
+        },
+        regions: {
+            region
+        }
+    });});
 
 //createAwsStandardAccount
 router.get('/create-standard', function(request, response) {
-    response.send({ result: 'success' });
+    response.send({
+        result: 'success',
+        route: '/create-standard',
+        instanceData: {
+            InstanceId,
+            KeyName, 
+            Architecture 
+        },
+        allocationIds: {
+            standard: "standard"
+        },
+        regions: {
+            region
+        }
+    });
 });
 
 //associateElasticIP
-router.get('/create-standard', function(request, response) {
+router.get('/associate-elastic-ip?instanceId=xxx&allocationId=yyy&region=zzz', function(request, response) {
+    response.send({ result: 'success' });
+    const values = qs.parse(this.props.location.search);
+    response.send({
+        result: 'success',
+        route: '/associate-elastic-ip' + values,
+        InstanceId: values.instanceId,
+        allocationId: values.allocationId,
+        regions: values.region
+    })
+
+});
+
+router.get('/get-instance-status?instanceId=xxx', function(request, response) {
+    const values = qs.parse(this.props.location.search);
+    response.send({
+        result: 'success',
+        route: '/get-instance-status' + values,
+        InstanceId: values.InstanceId
+        })
+});
+
+router.get('/reboot-instance', function(request, response) {
     response.send({ result: 'success' });
 });
+
+
 
 module.exports = router;
---
+++ aws-provision/server/routes/ssh-runner.js
@@ -0,0 +1,74 @@
+const Client = require('ssh2').Client;
+const qs = require('query-string');
+const elfUtils = require('elven-code').elfUtils;
+const hostAddress = '35.175.4.96';
+
+let allData = '';
+
+const runCpuInfo = (hostAddress, response) => {
+    var conn = new Client();
+    conn.on('ready', function() {
+        console.log('Client :: ready');
+        conn.exec('~/CpuInfo', function(err, stream) {
+            if (err) throw err;
+            stream
+                .on('close', function(code, signal) {
+                    console.log(
+                        'Stream :: close :: code: ' +
+                            code +
+                            ', signal: ' +
+                            signal
+                    );
+                    conn.end();
+                    response.send({ result: 'success', allData: allData });
+                })
+                .on('data', function(data) {
+                    console.log('STDOUT: ' + data);
+                    allData += data;
+                })
+                .stderr.on('data', function(data) {
+                    console.log('STDERR: ' + data);
+                    allData += data;
+                });
+        });
+    })
+    
+    .connect({
+        host: hostAddress,
+        port: 22,
+        username: 'ubuntu',
+        privateKey: require('fs').readFileSync(
+            process.env.HOME + '/.ssh/isit320-rackley.pem'
+        )
+    });
+};
+
+
+router.get('/run-get-started', function(request, response) {
+    console.log('run-get-started called in ssh-runner', hostAddress);
+    getSshIp()
+        .then((result) => {
+            runUptime(result.hostName, result.identityFile, response);
+        })
+        .catch((err) => {
+            response.send(err);
+        });
+
+});
+
+
+
+router.get('/call-cpu-info', (request, response) => {
+    response.send({ result: 'success' });
+});
+
+router.get('/run-ubuntu-setup', (request, response) => {
+    response.send({
+        result: 'success',
+        route: '/run-ubuntu-setup'
+    });
+});
+
+runCpuInfo(hostAddress, response);
+
+module.exports = router;
